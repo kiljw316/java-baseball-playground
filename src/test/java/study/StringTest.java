9e511e5d84b5ff74e867cd81cfc4d415fc6d33a1
kiljaewon <kiljw316@gmail.com>
2024-04-02 08:27:12 +0900

    String 학습테스트 요구사항 1

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,5 +1,6 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -10,4 +11,33 @@ public class StringTest {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
     }
+
+    @DisplayName("\"1,2\"을 ,로 split 했을 때 1과 2로 잘 분리되는지 확인하는 학습 테스트를 구현한다.")
+    @Test
+    void split_test() {
+        //given
+        String value = "1,2";
+
+        //when
+        String[] result = value.split(",");
+
+        //then
+        assertThat(result).contains("1", "2");
+        assertThat(result).hasSize(2);
+    }
+
+    @DisplayName("\"1\"을 ,로 split 했을 때 1만을 포함하는 배열이 반환되는지에 대한 학습 테스트를 구현한다.")
+    @Test
+    void single_array_split_test() {
+        //given
+        String value = "1";
+
+        //when
+        String[] result = value.split(",");
+
+        //then
+        assertThat(result).contains("1");
+        assertThat(result).hasSize(1);
+    }
+
 }
