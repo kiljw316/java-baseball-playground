23dae315f5de1f7080b3b2e09b41a8663045fe4e
kiljaewon <kiljw316@gmail.com>
2024-04-02 08:45:59 +0900

    Set Collection 학습테스트 요구사항 2

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -3,6 +3,9 @@ package study;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -31,5 +34,17 @@ public class SetTest {
 
         //then
         assertThat(result).isEqualTo(3);
+    }
+
+    @DisplayName("Set의 contains() 메소드를 활용해 1, 2, 3의 값이 존재하는지를 확인하는 학습테스트를 구현하려한다.")
+    @ParameterizedTest
+    @CsvSource({"1, true", "2, true", "3, true", "4, false"})
+    void contains_test(int num, boolean isContain) {
+        //given
+
+        //when
+
+        //then
+        assertThat(numbers.contains(num)).isEqualTo(isContain);
     }
 }
