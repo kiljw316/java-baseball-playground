bfa66f0ff718abb70caaba83f1cfb332ee8fdf9f
kiljaewon <kiljw316@gmail.com>
2024-04-02 08:36:58 +0900

    String 학습테스트 요구사항 3

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringTest {
     @Test
@@ -52,4 +53,35 @@ public class StringTest {
         //then
         assertThat(result).isEqualTo("1,2");
     }
+
+    @DisplayName("\"abc\" 값이 주어졌을 때 String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져오는 학습 테스트를 구현한다.")
+    @Test
+    void charAt_test() {
+        String value = "abc";
+
+        assertCharAt(value, 0, 'a');
+        assertCharAt(value, 1, 'b');
+        assertCharAt(value, 2, 'c');
+    }
+
+    @DisplayName("String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져올 때 위치 값을 벗어나면 StringIndexOutOfBoundsException이 발생하는 부분에 대한 학습 테스트를 구현한다.")
+    @Test
+    void chatAt_StringIndexOutOfBoundsException_test() {
+        //given
+        String value = "abc";
+
+        //when - then
+        assertThatThrownBy(() -> value.charAt(value.length()))
+                .isInstanceOf(StringIndexOutOfBoundsException.class)
+                .hasMessage("String index out of range: %s", value.length());
+    }
+
+    private void assertCharAt(String value, int index, char expected) {
+        //when
+        char result = value.charAt(index);
+
+        //then
+        assertThat(result).isEqualTo(expected);
+    }
+
 }
